f6e396bb0f6c00757c8e760c306a009b4208eb21
ThedeBothmann <53815388+ThedeBothmann@users.noreply.github.com>
2019-08-12 09:49:59 +0200

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,13 +1,13 @@
 package de.meinefirma.meinprojekt;
 
 public class Fibonacci {
-    public static void main(String[]args){
+    public static void main(String [] args){
         int a = 0;
         int b = 1;
         int c = b;
         for(int i=1;i<=25;i++){
             c = b;
-            b=a+b;
+            b= a+b;
             a = c;
             System.out.println(a);
         }
